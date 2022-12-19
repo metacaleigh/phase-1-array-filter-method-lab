8e7d9e15c5b315b71c7cf85a35002b23b89b46d7
caleigh <caleighsteill@outlook.com>
2022-12-18 21:44:32 -0700

    commit tree cad491550d706238619184c857ac86e1c0f030d1

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+// drivers.filter(function findMatching(driver) {
+//     return
+const array = ["Bob", "Chip", "Charlie"]
+
+function findMatching(arrayOfDrivers, string) {
+    return arrayOfDrivers.filter(driver => {
+        return driver.toLowerCase() === string.toLowerCase()
+    })
+}
+
+function fuzzyMatch(arrayOfDrivers, string) {
+    return arrayOfDrivers.filter(driver => {
+        return driver.startsWith(string)
+    })
+}
+console.log(fuzzyMatch(array, "C"))
+
+function matchName(arrayOfDrivers, string) {
+    return arrayOfDrivers.filter(driver => {
+        return driver.name === string
+    })
+}
